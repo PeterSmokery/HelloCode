5360db3b5f9814d8e54760e41178e58ce3ca6db8
PeterSmokery <PeterSmokery@gmail.com>
2021-11-22 20:49:39 +0300

    task 3 complete

---
+++ tasks/task 3/Program.cs
@@ -0,0 +1,28 @@
+﻿//eto task 4
+Console.Write("введите номер дня недели:");
+int now_day_is = Console.Read();
+
+if(now_day_is=='1')
+{
+Console.WriteLine("этот день Понедельник");
+}
+if(now_day_is=='2')
+{
+Console.WriteLine("этот день Вторник");
+}if(now_day_is=='3')
+{
+Console.WriteLine("этот день Среда");
+}if(now_day_is=='4')
+{
+Console.WriteLine("этот день Четверг");
+}if(now_day_is=='5')
+{
+Console.WriteLine("этот день Пятница");
+}if(now_day_is=='6')
+{
+Console.WriteLine("этот день Суббота ");
+}
+if(now_day_is=='7')
+{
+Console.WriteLine("этот день Воскресение");
+}
