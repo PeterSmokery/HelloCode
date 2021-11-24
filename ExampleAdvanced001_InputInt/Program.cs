fd78466ff0172cc1d5ab9fa787efac90c79848cf
PeterSmokery <PeterSmokery@gmail.com>
2021-11-24 10:03:56 +0300

    task 8 is done

--- ExampleAdvanced001_InputInt/Program.cs
+++ ExampleAdvanced001_InputInt/Program.cs
@@ -1,5 +1,5 @@
 ﻿Console.Write("Enter number: ");
 
-int num = int.Parse(Console.ReadLine());
+int num = int.Parse(Console.ReadLine());//перевод текста в число
 
 Console.WriteLine("num - {0}", num);  
---
+++ tasks/task 9/Program.cs
@@ -0,0 +1,3 @@
+﻿Console.WriteLine("введите трёхзначное число:");
+int N = int.Parse(Console.ReadLine());
+Console.WriteLine(N%10);
