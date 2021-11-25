1a8155988ca25ab5fd8c242b8368bcb932ffd5d7
PeterSmokery <PeterSmokery@gmail.com>
2021-11-25 20:11:19 +0300

    task 15 dont

---
+++ tasks/taks 15/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("введите число для сравнения ");
+int N =int.Parse(Console.ReadLine());
+if(N%7==0)
+{
+    Console.WriteLine("число кратно 7");
+}
+if(N%23==0)
+{
+    Console.WriteLine("число кратно 23");
+}
