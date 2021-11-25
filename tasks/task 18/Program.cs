369403d01205c4e5a77542b3823bf33f423358d8
PeterSmokery <PeterSmokery@gmail.com>
2021-11-25 21:59:48 +0300

    task 18 update

---
+++ tasks/task 18/Program.cs
@@ -0,0 +1,12 @@
+﻿//Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y
+bool X = true;
+bool y = true;
+bool Z = !(X & y)==!(X|!y);
+if(Z==false)
+{
+    Console.WriteLine("увтреждение ложно ");
+}
+else
+{
+    Console.WriteLine("утверждение истино");
+}
