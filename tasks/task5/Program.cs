d2627be3c2b70f2c8ff9a4553bfda395d73dd2ee
PeterSmokery <PeterSmokery@gmail.com>
2021-11-25 20:35:00 +0300

    task 5 reworked

--- tasks/task5/Program.cs
+++ tasks/task5/Program.cs
@@ -1,5 +1,7 @@
 ﻿Console.WriteLine("введите x для функции y=2*x+3");
 int x =int.Parse(Console.ReadLine());
 int y =  2*x+3;
-Console.Write("Значение функции равно:");
+Console.Write("Значение функции при X=");
+Console.Write(x);
+Console.Write(" равно ");
 Console.Write(y);
