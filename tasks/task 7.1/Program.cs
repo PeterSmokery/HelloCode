af032ddb9187689aa2a21253969a83b005a7c3dd
PeterSmokery <PeterSmokery@gmail.com>
2021-11-25 21:13:19 +0300

    task 7.1 complete

---
+++ tasks/task 7.1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("введите число N");
+int N =int.Parse(Console.ReadLine());
+
+int count = 0;
+while(count<N+1)
+{
+    if(count%2==0)
+{
+    Console.Write(count);
+    Console.Write(",");
+}
+count++;
+}
