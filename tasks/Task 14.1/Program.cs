2e623fea83f1119f54b11661f117931d87a0d17d
PeterSmokery <PeterSmokery@gmail.com>
2021-11-25 23:09:12 +0300

    task 14 update

---
+++ tasks/Task 14.1/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("введите число:");
+int N = int.Parse(Console.ReadLine());
+if(N<0)
+{
+    N=N*(-1);
+}
+int N0 = N;
+int Count_cifr = 0;
+while(N!=0)
+{
+    N = N/10;
+    Count_cifr++;
+}
+if (Count_cifr<3)
+{
+    Console.WriteLine("число не подходит");
+}
+else 
+{
+    while(Count_cifr>3)
+    {
+        N0=N0/10;
+        Count_cifr--;
+    }
+   Console.WriteLine(N0%10);
+}
