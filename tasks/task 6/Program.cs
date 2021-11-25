2f8497b8bd0a96baf2e1756a752176de2ff8859d
PeterSmokery <PeterSmokery@gmail.com>
2021-11-25 20:42:54 +0300

    task 6 complete

---
+++ tasks/task 6/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("введите число для проверки: ");
+int N =int.Parse(Console.ReadLine());
+if(N%2==0)
+{
+    Console.WriteLine("число чётное");
+}
+else
+{
+    Console.WriteLine("число нечётное");
+}
