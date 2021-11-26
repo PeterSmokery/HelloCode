cd5fbea686c76ef8ac72c63f89bef3b44d97eeca
PeterSmokery <PeterSmokery@gmail.com>
2021-11-26 03:34:35 +0300

    task 21 done

---
+++ tasks/task 21/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("введите пятизначное число на проверку:");
+int Number =int.Parse(Console.ReadLine());
+if ((Number%10==Number/10000)&(((Number/1000)%10==(Number%100)/10)))
+{
+    Console.WriteLine("да это же палиндром ! !  !");
+}
+else
+{
+    Console.WriteLine("не ,это не палиндром(");
+}
