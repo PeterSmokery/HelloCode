e24354d8a8919549ec823e07284b499479f0591d
PeterSmokery <PeterSmokery@gmail.com>
2021-11-26 03:23:07 +0300

    task 20 done

---
+++ tasks/task 20/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("задайте номер четверти:");
+int number =int.Parse(Console.ReadLine());
+if (number==1)
+{
+    Console.WriteLine("X>0 и Y>0");
+}
+if (number==2)
+{
+Console.WriteLine("X<0 и Y>0");
+}
+if (number==3)
+{
+Console.WriteLine("X<0 и Y<0");
+}
+if (number==4)
+{
+Console.WriteLine("X>0 и Y<0");
+}
+if ((number<0)^(number>4))
+{
+    Console.WriteLine("такой четверти плоскости не существует");
+}
