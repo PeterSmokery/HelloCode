32e0ba15e489a5df4285ecb2ad0e3531247d3c3b
PeterSmokery <PeterSmokery@gmail.com>
2021-11-26 03:12:01 +0300

    task 18 done

---
+++ tasks/task 19/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("введите координату X:");
+int X =int.Parse(Console.ReadLine());
+Console.WriteLine("введите координату Y:");
+int Y =int.Parse(Console.ReadLine());
+if ((X>0)&(Y>0))
+{
+    Console.WriteLine("точка находится в 1-ой четверти");
+}
+if ((X>0)&(Y<0))
+{
+     Console.WriteLine("точка находится в 4-ой четверти");
+}
+if ((X<0)&(Y<0))
+{
+     Console.WriteLine("точка находится в 3-ой четверти");
+}
+if ((X<0)&(Y>0))
+{
+     Console.WriteLine("точка находится в 2-ой четверти");
+}
