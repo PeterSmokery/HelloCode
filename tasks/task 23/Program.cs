5a36d93a215c49fc810daeeab6d40615bc36f49f
PeterSmokery <PeterSmokery@gmail.com>
2021-11-29 11:28:15 +0300

    task 23  done

---
+++ tasks/task 23/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("введите число N:");
+int N =int.Parse(Console.ReadLine());
+int count = 1;
+while (count-1<N)
+{
+    Console.Write(Math.Pow(count,2));
+    Console.Write(",");
+    count++;
+}
