2a8858f9215fa13cd5fdb77c751b3ada72d1fbf4
PeterSmokery <PeterSmokery@gmail.com>
2021-11-29 12:54:50 +0300

    task 31  done

---
+++ tasks/task 31/Program.cs
@@ -0,0 +1,28 @@
+﻿int[] mass = new int[8];
+void mssivePull(int[] pop)
+{
+    int lenght=pop.Length;
+    int lon = 0;
+    while (lon < lenght)
+    {
+        pop[lon]=new Random().Next(1,100);
+        lon++;
+    }
+}
+void mssivePrint(int[] arrey)
+{
+     int pop=arrey.Length;
+     int order=0;
+     while(order<pop)
+     {
+         Console.Write(arrey[order]);
+         if (order+1==pop)
+         {
+             break;
+         }
+         Console.Write(",");
+         order++;
+     }
+}
+mssivePull(mass);
+mssivePrint(mass);
