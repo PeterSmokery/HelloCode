57ae9ecfaa6b5bb9811393bef689cf82df547203
PeterSmokery <PeterSmokery@gmail.com>
2021-11-29 12:12:35 +0300

    task 27  done

---
+++ tasks/task 27/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("введите число:");
+int N = int.Parse(Console.ReadLine());
+int Count_cifr = 0;
+while(N!=0)
+{
+    N = N/10;
+    Count_cifr++;
+}
+Console.WriteLine(Count_cifr);
