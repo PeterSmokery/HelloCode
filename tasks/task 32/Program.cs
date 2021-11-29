2a1896068aa35b96159cc9379d86524d6941c375
PeterSmokery <PeterSmokery@gmail.com>
2021-11-29 13:05:33 +0300

    task 32 done

---
+++ tasks/task 32/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] mass = new int[8];
+void mssivePrint(int[] arrey)
+{
+     int pop=arrey.Length;
+     int order=0;
+     while(order<pop)
+     {
+         Console.Write(arrey[order]);
+         if (order+1==pop)
+         {
+             break;
+         }
+         Console.Write(",");
+         order++;
+     }
+}
+mssivePrint(mass);
+
