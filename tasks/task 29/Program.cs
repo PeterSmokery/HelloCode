0cc3bf47748bcc98479e39f088abb790a4243953
PeterSmokery <PeterSmokery@gmail.com>
2021-11-29 12:40:36 +0300

    task 29  done

---
+++ tasks/task 29/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("введите число:");
+int N = int.Parse(Console.ReadLine());
+int proizv = 1;
+int count = 0;
+while(count<N)
+{
+    count++;
+    proizv = proizv *  count;
+}
+Console.WriteLine(proizv);
+
+
