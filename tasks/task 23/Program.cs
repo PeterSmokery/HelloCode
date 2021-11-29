352718ef7fb03d66e2075312bc264149bbbcb42e
PeterSmokery <PeterSmokery@gmail.com>
2021-11-29 11:31:37 +0300

    task 23  done

--- tasks/task 23/Program.cs
+++ tasks/task 23/Program.cs
@@ -3,6 +3,9 @@ int N =int.Parse(Console.ReadLine());
 int count = 1;
 while (count-1<N)
 {
+    Console.Write("число ");
+    Console.Write(count);
+    Console.Write(" в квадрате равно = ");
     Console.Write(Math.Pow(count,2));
     Console.Write(",");
     count++;
