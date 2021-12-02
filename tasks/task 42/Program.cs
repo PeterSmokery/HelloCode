ac498180b0190b268a9fb8ce741ea92d6142ef55
PeterSmokery <PeterSmokery@gmail.com>
2021-12-02 20:30:06 +0300

    task 41 done

--- tasks/task 42/Program.cs
+++ tasks/task 42/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void keybord_number_reader(int kolvo)
+{
+int try_s = 0;
+int count = 0;
+while(try_s < kolvo )
+{
+Console.WriteLine("введите число: ");
+int number=int.Parse(Console.ReadLine());
+if (number>0)
+{
+    count++;
+}
+try_s++;
+}
+if (count==1)
+{
+    Console.WriteLine("с клавиатуры введено одно число больше нуля");
+}
+else if (count>5)
+{
+    Console.WriteLine("с клавиатуры введено : "+count+ " чисел больше 0");
+}
+else if ((count<5)&&(count>1))
+{
+    Console.WriteLine("с клавиатуры введено "+count+" числа больше 0");
+}
+else
+{
+    Console.WriteLine("с клавиатуры не введено ни одного числа больше 0");
+}
+}
+Console.WriteLine("введите ,сколько чисел вы хотите набрать с клавиатуры");
+int N = int.Parse(Console.ReadLine());
+keybord_number_reader(N);
