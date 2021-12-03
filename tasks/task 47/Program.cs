acd711c3d10d5c6ba844e391f5687f8c97b02db5
PeterSmokery <PeterSmokery@gmail.com>
2021-12-04 00:28:09 +0300

    task 47 done

---
+++ tasks/task 47/Program.cs
@@ -0,0 +1,20 @@
+﻿int[] arrey ={1,3,4,5,-3,2,8,4,8};
+void arrey_copy(int[] massive)
+{
+    int N = arrey.Length;
+    int[] dude = new int[N];
+    int count =0;
+    while(count<N)
+    {
+        dude[count]=arrey[count];
+        Console.Write(dude[count]);
+        if (count+1==N)
+        {
+            break;
+        }
+        Console.Write(",");
+        count++;
+    }
+
+}
+arrey_copy(arrey);
