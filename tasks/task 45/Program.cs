e4585bcce9899c81f070392482d7667737697618
PeterSmokery <PeterSmokery@gmail.com>
2021-12-03 23:42:08 +0300

    task 45 done

---
+++ tasks/task 45/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("введите сколько чисел фиббоначи вы хотите увидеть:");
+int N =int.Parse(Console.ReadLine());
+int[] arrey = new int[N];
+int length = arrey.Length;
+int count =0;
+while (count<length)
+{
+  if ((count==1)|(count==0))
+  {
+      arrey[count]=1;
+  }
+  else
+  {
+      arrey[count]=arrey[count-1]+arrey[count-2];
+  }
+  Console.Write(arrey[count]+",");
+   count++;
+}
