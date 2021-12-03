608600bf3f64412b8f08e53e8210d29582404865
PeterSmokery <PeterSmokery@gmail.com>
2021-12-03 21:18:48 +0300

    task 44 done

---
+++ tasks/task 44/Program.cs
@@ -0,0 +1,24 @@
+﻿int k1= int.Parse(Console.ReadLine());
+int K2= int.Parse(Console.ReadLine());
+int B1= int.Parse(Console.ReadLine());
+int B2 = int.Parse(Console.ReadLine());
+int X0=0;
+while((k1*X0+B1)!=(K2*X0+B2))
+{
+    if (X0==100)
+    {
+        break;
+    }
+    X0++;
+}
+while((k1*X0+B1)!=(K2*X0+B2))
+{
+    X0=0;
+    if (X0==-100)
+    {
+     break;   
+    }
+    X0--;
+}   
+int y0 = k1*X0+B1;
+Console.WriteLine("точка пересечения равна ( "+X0+";"+y0+" )");
