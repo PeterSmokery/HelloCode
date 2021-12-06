fd0f5b7dc39f29de1bbad71f5b0bd4896e8a95d9
PeterSmokery <PeterSmokery@gmail.com>
2021-12-06 14:57:58 +0300

    task 48 done

---
+++ tasks/task 48/Program.cs
@@ -0,0 +1,27 @@
+﻿int m = int.Parse(Console.ReadLine());
+int n = int.Parse(Console.ReadLine());
+int[,] massive = new int[m,n];
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+       for (int j = 0; j < matr.GetLength(1); j++)
+       {
+            matr[i, j] = new Random().Next(1, 10);// [1; 10)
+       }
+    }
+}
+
+ void PrintArray(int[,] matr)
+ {
+     for (int i = 0; i < matr.GetLength(0); i++)
+     {
+         for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+       Console.WriteLine();
+   }
+}
+FillArray(massive);
+PrintArray(massive);
